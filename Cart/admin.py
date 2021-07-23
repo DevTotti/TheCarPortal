a4556a61682e6907a50839629674df3d6cb52eff
Paul <devtotti@gmail.com>
2021-07-23 15:32:32 +0100

    adding to admin

--- Cart/admin.py
+++ Cart/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Cart, CartItem
+from .models import Cart, CartItem, Message
 # Register your models here.
 
 admin.site.register(Cart)
 admin.site.register(CartItem)
+admin.site.register(Message)
